9e40cf1bd091cb74877565b032e36aa6f51ff2a1
lkorchnoy <lkorchnoy@yahoo.com>
2020-05-29 19:15:37 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,13 +20,10 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
-  def search 
-    @students = Student.where('name LIKE ?', "%#{params[:query]}%")
-    render 'index'
-  end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
+    render :index
   end
 
   def student_params
